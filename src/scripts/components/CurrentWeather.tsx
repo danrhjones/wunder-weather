558b025af40ee94e4a4ba44f7d03c31269019872
Dan Jones <dan.rh.jones@gmail.com>
2022-07-11 21:07:01 +0100

    actualy fixed update pressure

--- src/scripts/components/CurrentWeather.tsx
+++ src/scripts/components/CurrentWeather.tsx
@@ -8,7 +8,7 @@ const convertToCelsius = (f: string | number) => {
 };
 
 const convertToHpa = (f: string | number) => {
-    return (f / 0.029529980164712).toFixed(2)
+    return (+f / 0.029529980164712).toFixed(2)
 }
 
 type CurrentWeatherProps = {
