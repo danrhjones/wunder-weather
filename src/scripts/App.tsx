c15b82bc558fd7ef96cafa78a199a68784eb42b2
Dan Jones <dan.rh.jones@gmail.com>
2022-07-13 23:06:24 +0100

    formatting and change moon phase a bit

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -5,76 +5,75 @@ import Header from './components/Header';
 import SunCalc from "suncalc";
 
 function App() {
-  const [weather, setWeather] = useState<any>();
-  const [weatherTime, setWeatherTime] = useState<Date>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [poop, setMoonPhase] = useState<any>();
-  // const [theme, setTheme] = useState<string>('theme-day-clear');
-  // const [daytime, setSun] = useState<boolean>(true); // WIP idea to track sunrise/sunset
+    const [weather, setWeather] = useState<any>();
+    const [weatherTime, setWeatherTime] = useState<Date>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [poop, setMoonPhase] = useState<any>();
+    // const [theme, setTheme] = useState<string>('theme-day-clear');
+    // const [daytime, setSun] = useState<boolean>(true); // WIP idea to track sunrise/sunset
 
 
-  const getCurrentWeather = () => {
-    const key = process.env.API_KEY;
-    const station = 'ILEIGH28';
-    fetch(`https://api.weather.com/v2/pws/observations/current?stationId=${station}&format=json&units=e&apiKey=${key}&numericPrecision=decimal`)
-      .then(response => response.json())
-      .then(data => {
-        setWeather(data.observations[0]);
-        setWeatherTime(new Date(data.observations[0].epoch * 1000));
-      })
-      .catch((error) => {
-        console.error(error);
-      }
-    );
-  }
+    const getCurrentWeather = () => {
+        const key = process.env.API_KEY;
+        const station = 'ILEIGH28';
+        fetch(`https://api.weather.com/v2/pws/observations/current?stationId=${station}&format=json&units=e&apiKey=${key}&numericPrecision=decimal`)
+            .then(response => response.json())
+            .then(data => {
+                setWeather(data.observations[0]);
+                setWeatherTime(new Date(data.observations[0].epoch * 1000));
+            })
+            .catch((error) => {
+                    console.error(error);
+                }
+            );
+    }
 
     const phase = () => {
         setMoonPhase(SunCalc.getMoonIllumination(new Date()).phase.toFixed(2))
-        //
     }
 
-  useEffect(() => {
-    getCurrentWeather();
-    setLoading(false);
-      phase()
-  }, []);
+    useEffect(() => {
+        getCurrentWeather();
+        setLoading(false);
+        phase()
+    }, []);
 
-  if (loading || !weather) {
+    if (loading || !weather) {
+        return (
+            <div className='wrapper'>
+                <Header/>
+                <CurrentWeather/>
+            </div>
+        );
+    }
+    console.log('🌦 Weather API from Wunderground:', weather); //to show dad how the data looks
+
+    // WIP idea to track sunrise/sunset
+    // const setSun = (time: Date) => {
+    //   const sun = SunCalc.getTimes(time, weather.lat, weather.lon)
+    //   console.log('sunset = ', sun.sunset);
+    //   console.log('sunrise = ', sun.sunrise);
+    //   console.log('current time = ', time);
+    // }
+
+    // const shuttupTypescript = true; // TEMP!! ONLY FOR WIP WORK!
+    // if (!shuttupTypescript) { //silence TS unused errors
+    //   console.info('shuttup typescript', setTheme, theme, setSun);
+    // }
+
     return (
-      <div className='wrapper'>
-        <Header/>
-        <CurrentWeather/>
-      </div>
+        <div className='wrapper'>
+            <Header
+                station={weather.stationID}
+                time={weatherTime}
+            />
+            <CurrentWeather
+                time={weatherTime}
+                weather={weather}
+                moon={poop}
+            />
+        </div>
     );
-  }
-  console.log('🌦 Weather API from Wunderground:', weather); //to show dad how the data looks
-
-  // WIP idea to track sunrise/sunset
-  // const setSun = (time: Date) => {
-  //   const sun = SunCalc.getTimes(time, weather.lat, weather.lon)
-  //   console.log('sunset = ', sun.sunset);
-  //   console.log('sunrise = ', sun.sunrise);
-  //   console.log('current time = ', time);
-  // }
-
-  // const shuttupTypescript = true; // TEMP!! ONLY FOR WIP WORK!
-  // if (!shuttupTypescript) { //silence TS unused errors
-  //   console.info('shuttup typescript', setTheme, theme, setSun);
-  // }
-
-  return (
-    <div className='wrapper'>
-      <Header
-        station={weather.stationID}
-        time={weatherTime}
-      />
-      <CurrentWeather
-        time={weatherTime}
-        weather={weather}
-        moon={poop}
-      />
-    </div>
-  );
 }
 
 export default App;
--- src/scripts/components/CurrentWeather.tsx
+++ src/scripts/components/CurrentWeather.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Weather } from '../../types';
+import {Weather} from '../../types';
 import {MoonModel} from "../../moonModel";
 
 const convertToCelsius = (f: string | number) => {
-  return (
-    ((+f - 32) * .5556).toFixed(1)
-  );
+    return (
+        ((+f - 32) * .5556).toFixed(1)
+    );
 };
 
 const convertToHpa = (f: string | number) => {
@@ -17,267 +17,267 @@ const convertInchesToMm = (f: string | number) => {
 }
 
 type CurrentWeatherProps = {
-  time?: Date;
-  weather?: Weather;
-  moon?: MoonModel;
+    time?: Date;
+    weather?: Weather;
+    moon?: MoonModel;
 };
 
 export default function CurrentWeather(props: CurrentWeatherProps) {
-  if (!props.weather) {
+    if (!props.weather) {
+        return (
+            <div className='weather-report'>
+                <WeatherItemLoading icon='thermostat' subject='Loading...'/>
+                <WeatherItemLoading icon='water_drop' subject='Loading...'/>
+                <WeatherItemLoading icon='air' subject='Loading...'/>
+                <WeatherItemLoading icon='explore' subject='Loading...'/>
+                <WeatherItemLoading icon='water_drop' subject='Loading...'/>
+                <WeatherItemLoading icon='opacity' subject='Loading...'/>
+                <WeatherItemLoading icon='light_mode' subject='Loading...'/>
+                <WeatherItemLoading icon='speed' subject='Loading...'/>
+                <WeatherItemLoading icon='dark_mode' subject='Loading...'/>
+            </div>
+        );
+    }
+
+    let tempFeel;
+    if (props.weather.imperial.temp >= 70) {
+        tempFeel = props.weather.imperial.heatIndex;
+    } else if (props.weather.imperial.temp <= 61) {
+        tempFeel = props.weather.imperial.windChill;
+    } else {
+        tempFeel = props.weather.imperial.temp;
+    }
+
     return (
-      <div className='weather-report'>
-        <WeatherItemLoading icon='thermostat' subject='Loading...' />
-        <WeatherItemLoading icon='water_drop' subject='Loading...' />
-        <WeatherItemLoading icon='air' subject='Loading...' />
-        <WeatherItemLoading icon='explore' subject='Loading...' />
-        <WeatherItemLoading icon='water_drop' subject='Loading...' />
-        <WeatherItemLoading icon='opacity' subject='Loading...' />
-        <WeatherItemLoading icon='light_mode' subject='Loading...' />
-        <WeatherItemLoading icon='speed' subject='Loading...' />
-        <WeatherItemLoading icon='dark_mode' subject='Loading...' />
-      </div>
+        <div className='weather-report'>
+            <WeatherItem
+                subject='Temperature'
+                icon='thermostat'
+                temp={props.weather.imperial.temp || '0'}
+                tempFeel={tempFeel}
+                unit='metric'
+            />
+            <WeatherItem
+                subject='Rain'
+                icon='water_drop'
+                rain={props.weather.imperial.precipTotal || '0'}
+                rainRate={props.weather.imperial.percipRate || '0'}
+                unit='metric'
+            />
+            <WeatherItem
+                subject='Wind'
+                icon='air'
+                unit='metric'
+                windGust={props.weather.imperial.windGust || '0'}
+                windSpeed={props.weather.imperial.windSpeed || '0'}
+            />
+            <WeatherItem
+                subject='Wind Direction'
+                icon='explore'
+                unit='metric'
+                windDir={props.weather.winddir || 0}
+            />
+            <WeatherItem
+                subject='Humidity'
+                icon='water_drop'
+                humidity={props.weather.humidity || '0'}
+                unit='metric'
+                error={
+                    props.weather.humidity === 1 ? 'Hardware malfunction.' : undefined
+                }
+            />
+            <WeatherItem
+                subject='Dew Point'
+                icon='opacity'
+                dewPoint={props.weather.imperial.dewpt || '0'}
+                unit='metric'
+            />
+            <WeatherItem
+                subject='Air Pressure'
+                icon='speed'
+                pressure={props.weather.imperial.pressure || '0'}
+                unit='imperial'
+            />
+            <WeatherItem
+                subject='Moon Phase'
+                icon='dark_mode'
+                moonPhase={props.moon || 0}
+                unit='imperial'
+            />
+        </div>
     );
-  }
-
-  let tempFeel;
-  if (props.weather.imperial.temp >= 70) {
-    tempFeel = props.weather.imperial.heatIndex;
-  } else if (props.weather.imperial.temp <= 61) {
-    tempFeel = props.weather.imperial.windChill;
-  } else {
-    tempFeel = props.weather.imperial.temp;
-  }
-
-  return (
-    <div className='weather-report'>
-      <WeatherItem
-        icon='thermostat'
-        subject='Temperature'
-        temp={props.weather.imperial.temp || '0'}
-        tempFeel={tempFeel}
-        unit='metric'
-      />
-      <WeatherItem
-        subject='Rain'
-        icon='water_drop'
-        rain={props.weather.imperial.precipTotal || '0'}
-        rainRate={props.weather.imperial.percipRate || '0'}
-        unit='metric'
-      />
-      <WeatherItem
-        subject='Wind'
-        icon='air'
-        unit='metric'
-        windGust={props.weather.imperial.windGust || '0'}
-        windSpeed={props.weather.imperial.windSpeed || '0'}
-      />
-      <WeatherItem
-        subject='Wind Direction'
-        icon='explore'
-        unit='metric'
-        windDir={props.weather.winddir || 0}
-      />
-      <WeatherItem
-        subject='Humidity'
-        icon='water_drop'
-        humidity={props.weather.humidity || '0'}
-        unit='metric'
-        error={
-          props.weather.humidity === 1 ? 'Hardware malfunction.' : undefined
-        }
-      />
-      <WeatherItem
-        subject='Dew Point'
-        icon='opacity'
-        dewPoint={props.weather.imperial.dewpt || '0'}
-        unit='metric'
-      />
-      <WeatherItem
-        subject='Air Pressure'
-        icon='speed'
-        pressure={props.weather.imperial.pressure || '0'}
-        unit='imperial'
-      />
-      <WeatherItem
-        subject='Moon Phase'
-        icon='dark_mode'
-        moonPhase={ props.moon || 0}
-        unit='imperial'
-      />
-    </div>
-  );
 }
 
 type WeatherItemLoadingProps = {
-  icon: string;
-  subject: string;
+    icon: string;
+    subject: string;
 };
 
 const WeatherItemLoading = (props: WeatherItemLoadingProps) => {
-  return (
-    <div className='item'>
-      <h2>{props.subject}</h2>
-      <span
-        aria-hidden='true'
-        className={`material-icons${props.icon !== 'explore' ? '-outlined' : ''}`}
-      >
+    return (
+        <div className='item'>
+            <h2>{props.subject}</h2>
+            <span
+                aria-hidden='true'
+                className={`material-icons${props.icon !== 'explore' ? '-outlined' : ''}`}
+            >
         {props.icon}
       </span>
-      <p>0</p>
-    </div>
-  );
+            <p>0</p>
+        </div>
+    );
 };
 
 type WeatherItemProps = {
-  dewPoint?: number | string;
-  error?: string;
-  humidity?: number | string;
-  icon: string;
-  pressure?: number | string;
-  rain?: number | string;
-  rainRate?: number | string;
-  subject: string;
-  temp?: number | string;
-  tempFeel?: number | string;
-  unit: 'imperial' | 'metric';
-  uv?: number | string;
-  windGust?: number | string;
-  windSpeed?: number | string;
-  windDir?: number;
-  moonPhase?: any
+    dewPoint?: number | string;
+    error?: string;
+    humidity?: number | string;
+    icon: string;
+    pressure?: number | string;
+    rain?: number | string;
+    rainRate?: number | string;
+    subject: string;
+    temp?: number | string;
+    tempFeel?: number | string;
+    unit: 'imperial' | 'metric';
+    uv?: number | string;
+    windGust?: number | string;
+    windSpeed?: number | string;
+    windDir?: number;
+    moonPhase?: any
 };
 
 const WeatherItem = (props: WeatherItemProps) => {
-  const windDirection = (dir: number) => {
-    if ((dir > 349 && dir <= 360) || dir <= 11) {
-      return 'North';
-    } else if (dir > 11 && dir <= 34) {
-      return 'North North East';
-    } else if (dir > 34 && dir <= 56) {
-      return 'North East';
-    } else if (dir > 56 && dir <= 79) {
-      return 'East North East';
-    } else if (dir > 79 && dir <= 101) {
-      return 'East';
-    } else if (dir > 101 && dir <= 124) {
-      return 'East South East';
-    } else if (dir > 124 && dir <= 146) {
-      return 'South East';
-    } else if (dir > 146 && dir <= 169) {
-      return 'South South East';
-    } else if (dir > 169 && dir <= 191) {
-      return 'South';
-    } else if (dir > 191 && dir <= 214) {
-      return 'South South West';
-    } else if (dir > 214 && dir <= 236) {
-      return 'South West';
-    } else if (dir > 236 && dir <= 259) {
-      return 'West South West';
-    } else if (dir > 259 && dir <= 281) {
-      return 'West';
-    } else if (dir > 281 && dir <= 304) {
-      return 'West North West';
-    } else if (dir > 304 && dir <= 326) {
-      return 'North West';
-    } else if (dir > 326 && dir <= 349) {
-      return 'North North West';
-    }
-    return 'ERROR';
-  };
+    const windDirection = (dir: number) => {
+        if ((dir > 349 && dir <= 360) || dir <= 11) {
+            return 'North';
+        } else if (dir > 11 && dir <= 34) {
+            return 'North North East';
+        } else if (dir > 34 && dir <= 56) {
+            return 'North East';
+        } else if (dir > 56 && dir <= 79) {
+            return 'East North East';
+        } else if (dir > 79 && dir <= 101) {
+            return 'East';
+        } else if (dir > 101 && dir <= 124) {
+            return 'East South East';
+        } else if (dir > 124 && dir <= 146) {
+            return 'South East';
+        } else if (dir > 146 && dir <= 169) {
+            return 'South South East';
+        } else if (dir > 169 && dir <= 191) {
+            return 'South';
+        } else if (dir > 191 && dir <= 214) {
+            return 'South South West';
+        } else if (dir > 214 && dir <= 236) {
+            return 'South West';
+        } else if (dir > 236 && dir <= 259) {
+            return 'West South West';
+        } else if (dir > 259 && dir <= 281) {
+            return 'West';
+        } else if (dir > 281 && dir <= 304) {
+            return 'West North West';
+        } else if (dir > 304 && dir <= 326) {
+            return 'North West';
+        } else if (dir > 326 && dir <= 349) {
+            return 'North North West';
+        }
+        return 'ERROR';
+    };
 
-  const moonPhaseIcon = (phase: number) => {
-    if (phase == 0) {
-        return 'New Moon';
-    } else if (phase > 0 && phase <= 0.24) {
-        return 'Waxing Cresent';
-    } else if (phase == 0.25 ) {
-        return 'First Quarter';
-    } else if (phase > 0.25 && phase <= 0.49) {
-        return 'Waxing Cresent';
-    } else if (phase == 0.5 ) {
-        return 'Full Moon';
-    } else if (phase > 0.5 && phase <= 0.74) {
-        return 'Waning Gibbous';
-    } else if (phase == 0.75 ) {
-        return 'Last Quarter';
-    } else if (phase > 0.75 && phase <= 0.99) {
-        return 'Waning Cresent';
-    }
-    return 'ERROR';
-};
+    const moonPhaseIcon = (phase: number) => {
+        if (phase >= 0 && phase <= 0.09) {
+            return 'New Moon';
+        } else if (phase > 0.10 && phase <= 0.23) {
+            return 'Waxing Cresent';
+        } else if (phase >= 0.25 && phase <= 0.27) {
+            return 'First Quarter';
+        } else if (phase > 0.28 && phase <= 0.47) {
+            return 'Waxing Cresent';
+        } else if (phase >= 0.48 && phase <= 0.54) {
+            return 'Full Moon';
+        } else if (phase >= 0.55 && phase <= 0.71) {
+            return 'Waning Gibbous';
+        } else if (phase >= 0.72 && phase <= 0.78) {
+            return 'Last Quarter';
+        } else if (phase > 0.79 && phase <= 0.99) {
+            return 'Waning Cresent';
+        }
+        return 'ERROR';
+    };
 
     return (
-    <div className={props.error ? 'item itemError' : 'item'}>
-      <h2>{props.subject}</h2>
-      <span
-        aria-hidden='true'
-        className={`material-icons${props.icon !== 'explore' ? '-outlined' : ''}`}
-      >
+        <div className={props.error ? 'item itemError' : 'item'}>
+            <h2>{props.subject}</h2>
+            <span
+                aria-hidden='true'
+                className={`material-icons${props.icon !== 'explore' ? '-outlined' : ''}`}
+            >
         {props.icon}
       </span>
 
-      {props.temp && props.tempFeel ? (
-        <>
-            <p>{`${convertToCelsius(props.temp)}°C`}</p>
-          <p>{`feels like ${convertToCelsius(
-            props.temp
-          )}°C`}</p>
-        </>
-      ) : null}
+            {props.temp && props.tempFeel ? (
+                <>
+                    <p>{`${convertToCelsius(props.temp)}°C`}</p>
+                    <p>{`feels like ${convertToCelsius(
+                        props.temp
+                    )}°C`}</p>
+                </>
+            ) : null}
 
-      {props.rain && props.rainRate ? (
-        <>
-          <p>{`${convertInchesToMm(props.rain)} mm`}</p>
-          <p>
-              {props.rainRate == 0 ? '' : `at a rate of ${convertInchesToMm(props.rainRate)} mm/hr` }
-          </p>
-        </>
-      ) : null}
+            {props.rain && props.rainRate ? (
+                <>
+                    <p>{`${convertInchesToMm(props.rain)} mm`}</p>
+                    <p>
+                        {props.rainRate == 0 ? '' : `at a rate of ${convertInchesToMm(props.rainRate)} mm/hr`}
+                    </p>
+                </>
+            ) : null}
 
-      {props.humidity && !props.error ? <p>{`${props.humidity}%`}</p> : null}
+            {props.humidity && !props.error ? <p>{`${props.humidity}%`}</p> : null}
 
-      {props.windGust && props.windSpeed ? (
-        <>
-          <p>{`${props.windSpeed} mph`}</p>
-          <p>
-            {props.windSpeed !== '0'
-              ? `with ${props.windGust} mph gusts`
-              : 'No wind'}
-          </p>
-        </>
-      ) : null}
+            {props.windGust && props.windSpeed ? (
+                <>
+                    <p>{`${props.windSpeed} mph`}</p>
+                    <p>
+                        {props.windSpeed !== '0'
+                            ? `with ${props.windGust} mph gusts`
+                            : 'No wind'}
+                    </p>
+                </>
+            ) : null}
 
-      {props.windDir ? (
-        <>
-          <p>{`${windDirection(props.windDir)}`}</p>
-        </>
-      ) : null}
+            {props.windDir ? (
+                <>
+                    <p>{`${windDirection(props.windDir)}`}</p>
+                </>
+            ) : null}
 
-      {props.pressure ? (
-        <>
-          <p>{`${convertToHpa(props.pressure)} hPa`}</p>
-        </>
-      ) : null}
+            {props.pressure ? (
+                <>
+                    <p>{`${convertToHpa(props.pressure)} hPa`}</p>
+                </>
+            ) : null}
 
-      {props.uv ? (
-        <>
-          <p>{`${props.uv}`}</p>
-        </>
-      ) : null}
+            {props.uv ? (
+                <>
+                    <p>{`${props.uv}`}</p>
+                </>
+            ) : null}
 
-      {props.dewPoint ? (
-        <>
-          <p>{`${convertToCelsius(props.dewPoint)}°C`}</p>
-        </>
-      ) : null}
+            {props.dewPoint ? (
+                <>
+                    <p>{`${convertToCelsius(props.dewPoint)}°C`}</p>
+                </>
+            ) : null}
 
-    {props.moonPhase ? (
-        <>
-            <p>{`${moonPhaseIcon(props.moonPhase)}`}</p>
-        </>
-    ) : null}
+            {props.moonPhase ? (
+                <>
+                    <p>{`${moonPhaseIcon(props.moonPhase)}`}</p>
+                </>
+            ) : null}
 
-      {props.error ? <p className='itemErrorText'>{props.error}</p> : null}
-    </div>
-  );
+            {props.error ? <p className='itemErrorText'>{props.error}</p> : null}
+        </div>
+    );
 };
